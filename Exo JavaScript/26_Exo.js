08ad88917ac93be22e3a866778671eafca38561c
Lumar_ux <82873564+Lumar-ux@users.noreply.github.com>
2024-04-19 12:36:26 +0200

    Delete Exo JavaScript directory

--- Exo JavaScript/26_Exo.js
+++
@@ -1,5 +0,0 @@
-const numbers = [1, 2, 3, 4, 5];
-const squareNumbers = numbers.map(num => num * num);
-console.log(squareNumbers);
-const squareNumbers3 = numbers.forEach((num) => {console.log(num * num)});
-// const squareNumbers2 = numbers.forEach(function(num) {console.log(num * num)});
